e128a46098bc0866ae214775f556e47cb1868da6
Umang Kumar <88194464+umangkumarchaudhary@users.noreply.github.com>
2023-01-31 10:09:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const wordList = ["Viber", "WhatsApp", "Telegram", "Facebook", "Instagram"];
+const hints = ["A cross-platform instant messaging app", "A popular messaging app", "A cloud-based instant messaging app", "A social networking website", "A photo and video sharing app"];
+
+let word = wordList[Math.floor(Math.random() * wordList.length)];
+let hint = hints[wordList.indexOf(word)];
+let remainingGuesses = 8;
+let lettersGuessed = [];
+
+document.querySelector("#hint").innerHTML = `Hint: ${hint}`;
+document.querySelector("#submitBtn").addEventListener("click", function() {
+  let letterInput = document.querySelector("#letterInput").value;
+  if (letterInput.length > 1) {
+    alert("Please enter a single letter.");
+    return;
+  }
+  if (lettersGuessed.includes(letterInput)) {
+    alert("You have already tried that letter. Please try another one.");
+    return;
+  }
+  if (!word.includes(letterInput)) {
+    remainingGuesses--;
+    document.querySelector("#remainingGuesses").innerHTML = `Remaining Guesses: ${remainingGuesses}`;
+  }
+  lettersGuessed.push(letterInput);
+  updateDisplay();
+});
+
+export function updateDisplay() {
+  let wordDisplay = "";
+  for (let i = 0; i < word.length; i++) {
+    if (lettersGuessed.includes(word[i])) {
+      wordDisplay += word[i];
+    } else {
+      wordDisplay += "_";
+    }
+  }
+  document.querySelector("#word").innerHTML = wordDisplay;
+
+  if (!wordDisplay.includes("_")) {
+    document.querySelector("#message").innerHTML = "You win!";
+    document.querySelector("#letterInput").disabled = true;
+    document.querySelector("#submitBtn").disabled = true;
+  }
+  if (remainingGuesses === 0) {
+    document.querySelector("#message").innerHTML = `You lose! The word was ${word}.`;
+    document.querySelector("#letterInput").disabled = true;
+    document.querySelector("#submitBtn").disabled = true;
+  }
+}
